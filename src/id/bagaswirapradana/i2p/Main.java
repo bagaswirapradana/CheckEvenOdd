b778fa312935db4976ac9e11c0f0c2908e662337
Herzandhy Bagas Wira Pradana <bagaswirapradana@gmail.com>
2020-09-27 00:53:27 +0700

    initial commit

--- src/id/bagaswirapradana/i2p/Main.java
+++ src/id/bagaswirapradana/i2p/Main.java
@@ -1,19 +1,3 @@
-# Check Even or Odd
-##### Introduction to programming
-##### SESSION 5 - Selection (OL) #3
-
-
-**Task**
-
-Jika diberikan suatu bilangan bulat (n) dan syarat berikut:
-- Jika n ganjil, cetak "AJAIB".
-- Jika n genap dan masih dalam rentang 2 - 5, cetak "Tidak AJAIB".
-- Jika n genap dan masih dalam rentang 6 - 20, cetak "AJAIB".
-- Jika n genap dan lebih besar dari 20, cetak "Tidak AJAIB".
-
-Note: hanya untuk bilangan bulat positif Integer antara 1 - 100.
-
-```java
 package id.bagaswirapradana.i2p;
 
 import java.util.Scanner;
@@ -21,30 +5,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // variabel
         int value;
-        
+
+        // input
         Scanner masukan = new Scanner(System.in);
         System.out.print("Masukkan Angka antara 1 - 100 : ");
-        
+
+        // validation integer or not
         if (!masukan.hasNextInt()) {
             System.out.println("Data yang dimasukkan hanya bisa berupa bilangan bulat saja.");
             masukan.close();
         }
 
         value = masukan.nextInt();
+        // validation range 1 until 100 or not
         if (value < 1 || value > 100) {
             System.out.println("Data yang dimasukkan harus dalam rentang dari 1 sampai 100.");
         } else {
+            /*
+              - if n odd, print "AJAIB"
+              - if n even and still range of 2-5 print "Tidak AJAIB"
+              - if n even and stiil range of 6 - 20, print "AJAIB"
+              - if n even and more than 20, print "Tidak AJAIB"
+             */
             System.out.println(value % 2 != 0 || value >= 6 && value <= 20 ? "AJAIB" : "Tidak AJAIB");
         }
         masukan.close();
     }
 }
-```
-
-*Test Case 1*
-![Case 1](testcase1.png)
-
-*Test Case 2*
-![Case 2](testcase2.png)
-
